a4a88e04ec448365c302146adf4226c764628907
deepforce123 <deepforce06@gmail.com>
2024-08-05 01:09:04 +0100

    test

--- main.py
+++ main.py
@@ -60,7 +60,8 @@ class client(discord.Client):
     
     async def setup_hook(self) -> None:
         
-        weekly_quota_reminder.start()
+        self.weekly_quota_reminder.start()
+        print(self.weekly_quota_reminder.next_iteration())
         
         async with aiosqlite.connect(database) as db:
             #YYYY-MM-DD
@@ -82,7 +83,7 @@ class client(discord.Client):
                                 WeekStart TEXT,
                                 Pass INTEGER,
                                 FOREIGN KEY(WeekStart) REFERENCES Weeks(StartDate)
-                                )""") # -1 activity = doesn't apply
+                                )""")
             
             await db.execute("""CREATE TABLE IF NOT EXISTS SeniorInspections(
                                 ID INTEGER PRIMARY KEY, 
@@ -137,6 +138,43 @@ class client(discord.Client):
             self.synced = True
 
         print_green(f"Logged in as {self.user}.")
+    
+    weekly_reminder_time = time(hour=12)
+
+    @tasks.loop(time=weekly_reminder_time)
+    async def weekly_quota_reminder():
+        if datetime.now().weekday() == 0:
+            guild = aclient.get_guild(guild_id)
+            reminder_channel = get(guild.channels, id = 1173680917374578718)
+            
+            dt = datetime.now(datetime.UTC) - timedelta(days=7)
+            week_start = f"{dt.year}-{dt.month}-{dt.day}"
+            
+            async with aiosqlite.connect(database) as db:
+                async with db.execute("""SELECT InspectorID
+                                    FROM Inspections
+                                    WHERE WeekStart=?
+                                    ORDER BY PostsCompleted DESC""", (week_start,)) as cursor:
+                    results1 = await cursor.fetchall()
+            
+            
+            async with aiosqlite.connect(database) as db:
+                async with db.execute("""SELECT InspectorID 
+                                    FROM SeniorInspections
+                                    WHERE WeekStart=?
+                                    ORDER BY PostsCompleted DESC""", (week_start,)) as cursor:
+                    results2 = await cursor.fetchall()
+            
+            
+            loggedLoggers = results1+results2
+
+            expectedLoggers = [m.id for m in get(guild.roles, id = senior_role_id).members]
+            
+            missingLoggers = list(set(loggedLoggers).symmetric_difference(set(expectedLoggers)))
+            
+            if len(missingLoggers) > 0:
+                await reminder_channel.send(f"{",".join([f'<@{ml}>' for ml in missingLoggers])}\n\nQuotas should all be in by now. Last call.")
+    
         
 aclient = client()
 tree = app_commands.CommandTree(aclient)
@@ -283,41 +321,7 @@ async def Get_Consecutive_Strikes(staff_member : int): # only accurate if quota 
         
     return counter
 
-weekly_reminder_time = time(hour=12)
 
-@tasks.loop(time=weekly_reminder_time)
-async def weekly_quota_reminder():
-    if datetime.now().weekday() == 0:
-        guild = aclient.get_guild(guild_id)
-        reminder_channel = get(guild.channels, id = 1173680917374578718)
-        
-        dt = datetime.now(datetime.UTC) - timedelta(days=7)
-        week_start = f"{dt.year}-{dt.month}-{dt.day}"
-        
-        async with aiosqlite.connect(database) as db:
-            async with db.execute("""SELECT InspectorID
-                                FROM Inspections
-                                WHERE WeekStart=?
-                                ORDER BY PostsCompleted DESC""", (week_start,)) as cursor:
-                results1 = await cursor.fetchall()
-        
-        
-        async with aiosqlite.connect(database) as db:
-            async with db.execute("""SELECT InspectorID 
-                                FROM SeniorInspections
-                                WHERE WeekStart=?
-                                ORDER BY PostsCompleted DESC""", (week_start,)) as cursor:
-                results2 = await cursor.fetchall()
-        
-        
-        loggedLoggers = results1+results2
-
-        expectedLoggers = [m.id for m in get(guild.roles, id = senior_role_id).members]
-        
-        missingLoggers = list(set(loggedLoggers).symmetric_difference(set(expectedLoggers)))
-        
-        if len(missingLoggers) > 0:
-            await reminder_channel.send(f"{",".join([f'<@{ml}>' for ml in missingLoggers])}\n\nQuotas should all be in by now. Last call.")
 
 rewardGroup = Group(name = "reward", description= "Handle rewards", guild_ids=guild_id_l)
 
