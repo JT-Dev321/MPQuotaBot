c9e595d6b6fb3955adca480211f7ee5c41f02781
deepforce123 <deepforce06@gmail.com>
2024-08-18 14:53:30 +0100

    better

--- main.py
+++ main.py
@@ -579,7 +579,12 @@ async def get_date(interaction: discord.Interaction, id_csv : str = ""):
     for m in mondays:
         date = str(m).split("|")[0]
         i = str(m).split("|")[1]
-        output += f"`/quota department:Marketplace quota_start:{date} user_ids:{id_csv}` | `{i}` days ago\n"
+        output += f"`/quota department:Marketplace quota_start:{date}"
+        if len(id_csv) > 0:
+            output += f" user_ids:{id_csv}`"
+        else:
+            output += "`"
+        output += f" | `{i}` days ago\n"
         # /quota department:Marketplace quota_start: user_ids:
     await interaction.response.send_message(f"{output}", ephemeral=True)
 
