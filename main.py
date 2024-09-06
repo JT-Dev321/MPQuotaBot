28c8c2da598b4a16c67e052c62c485510cf71f07
deepforce123 <deepforce06@gmail.com>
2024-09-06 15:27:10 +0100

    experienced role giving

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ class bot(commands.Bot):
         print_green(self.guilds)
         print_green(f"Logged in as {self.user}.")
     
-    @tasks.loop(seconds=10)
+    @tasks.loop(hours=24)
     async def experienced_role_distribute(self):
         guild = myBot.get_guild(guild_id)
         print(guild.name)
