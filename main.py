6306336a9bb57d8320cf5cb306e825607bbfb38c
deepforce123 <deepforce06@gmail.com>
2024-09-06 15:10:35 +0100

    bosh# Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -292,7 +292,7 @@ class bot(commands.Bot):
     @tasks.loop(hours=24)
     async def experienced_role_distribute(self):
         guild = aclient.get_guild(guild_id)
-        for m in guild.members:
+        for m in list(guild.members):
             if m.joined_at < datetime.now(timezone.utc) - timedelta(days=365) and not m.bot:
                 await m.add_roles(get(aclient.get_guild(guild_id), id=1281614132419891200))
     
