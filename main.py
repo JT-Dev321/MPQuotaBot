032b78c54f78668ec6edb36765fb8996dac0fc37
deepforce123 <deepforce06@gmail.com>
2024-09-06 15:08:51 +0100

    fix

--- main.py
+++ main.py
@@ -293,7 +293,7 @@ class bot(commands.Bot):
     async def experienced_role_distribute(self):
         guild = aclient.get_guild(guild_id)
         for m in guild.members:
-            if m.joined_at < datetime.now() - timedelta(days=365) and not m.bot:
+            if m.joined_at < datetime.now(timezone.utc) - timedelta(days=365) and not m.bot:
                 await m.add_roles(get(aclient.get_guild(guild_id), id=1281614132419891200))
     
     weekly_reminder_time_before = time(hour=18, tzinfo=timezone.utc)
