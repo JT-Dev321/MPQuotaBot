c3298bf384211fc698f023ce5ceb727649a4e3f7
deepforce123 <deepforce06@gmail.com>
2024-09-16 15:49:09 +0100

    ids in parse

--- commands/quota/quota.py
+++ commands/quota/quota.py
@@ -20,7 +20,7 @@ class parse_data_modal(ui.Modal, title = 'Data parser'):
             output = ""
             
             for name in quotaDict:
-                output += f"`/quota log staff_member:{name} post_count:{quotaDict[name][0]} ticket_count:{quotaDict[name][1]} week_start: `\n"
+                output += f"`/quota log staff_member:{await interaction.guild.get_member_named(name).id} post_count:{quotaDict[name][0]} ticket_count:{quotaDict[name][1]} week_start: `\n"
         else:
             for i in range(0, len(splitData), 6):
                 quotaDict.update({f"{splitData[i]}" : int(splitData[i+4].split(': ')[1])})
@@ -28,8 +28,8 @@ class parse_data_modal(ui.Modal, title = 'Data parser'):
             output = ""
             
             for name in quotaDict:
-                output += f"`/quota log staff_member:{name} post_count:{quotaDict[name]} ticket_count:0 week_start: `\n"
-        
+                output += f"`/quota log staff_member:{await interaction.guild.get_member_named(name).id} post_count:{quotaDict[name]} ticket_count:0 week_start: `\n"
+
         await interaction.response.send_message(output, ephemeral=True)
         
 class quota(commands.GroupCog, group_name='quota', group_description='Manage quotas'):
