ee5af82f4078a3b4cfad79508a4a1c4056fc2199
deepforce123 <deepforce06@gmail.com>
2025-06-01 19:22:19 +0100

    Refactored checks

--- main.py
+++ main.py
@@ -896,7 +896,9 @@ async def top_performer(interaction: discord.Interaction, weeks : int = 4, amoun
             posts = {}
             tickets = {}
             for row in results:
-                if interaction.guild.get_member(int(row[0])) is None or myBot.IsSenior(int(row[0])):
+                if interaction.guild.get_member(int(row[0])) is None:
+                    continue
+                if myBot.IsSenior(int(row[0])):
                     continue
                 posts[int(row[0])] = int(row[1] if row[1] is not None else 0)
                 tickets[int(row[0])] = int(row[2] if row[2] is not None else 0)
