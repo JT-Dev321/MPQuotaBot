c811bcb59ff1018adeef9d49aa306e01eacf4211
deepforce123 <deepforce06@gmail.com>
2025-06-01 19:15:58 +0100

    Added senior check to top performer

--- main.py
+++ main.py
@@ -891,7 +891,7 @@ async def top_performer(interaction: discord.Interaction, weeks : int = 4, amoun
             posts = {}
             tickets = {}
             for row in results:
-                if interaction.guild.get_member(int(row[0])) is None:
+                if interaction.guild.get_member(int(row[0])) is None or myBot.IsSenior(interaction.guild.get_member(int(row[0]))):
                     continue
                 posts[int(row[0])] = int(row[1] if row[1] is not None else 0)
                 tickets[int(row[0])] = int(row[2] if row[2] is not None else 0)
